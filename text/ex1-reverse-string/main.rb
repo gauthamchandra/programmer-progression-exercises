efedb4609eff346ab2df6c15445468d646ca9115
Gautham Chandra <gautham.chandra@greenhouse.io>
2019-01-14 17:18:30 -0500

    Provide solution to text/ex1

--- text/ex1-reverse-string/main.rb
+++ text/ex1-reverse-string/main.rb
@@ -1,6 +1,20 @@
 # frozen_string_literal: true
 
 def reverse(input)
-  # Add code here
-  raise NotImplementedError
+  return input if input.size == 1
+
+  i = 0
+  j = input.size - 1
+
+  reversed_str = String.new(input)
+
+  while j > input.size / 2
+    copy = reversed_str[i]
+    reversed_str[i] = reversed_str[j]
+    reversed_str[j] = copy
+    i += 1
+    j -= 1
+  end
+
+  reversed_str
 end
