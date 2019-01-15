769066efcd9fac3ab871a71dbbdb56b2a588f4e9
Muntasir Hasnat <mhasnat@greenhouse.io>
2019-01-14 23:40:53 -0500

    wrote method

--- text/ex1-reverse-string/main.rb
+++ text/ex1-reverse-string/main.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 def reverse(input)
-  # Add code here
-  raise NotImplementedError
+   reverse_input = input.length - 1 
+   while reverse_input >= 0 
+   	return input[reverse_input]
+   	reverse_input -= 1
+   end
+   # raise NotImplementedError
 end
