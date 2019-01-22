4f6ef9d8f92ea6ee4916259e0293f10c83bd6309
Muntasir Hasnat <mhasnat@greenhouse.io>
2019-01-22 18:04:14 -0500

    add count method

--- text/ex2-count-words-in-file/main.rb
+++ text/ex2-count-words-in-file/main.rb
@@ -1,5 +1,16 @@
 # frozen_string_literal: true
 
 def count(input)
-  raise NotImplementedError
+  input = input
+  a = input.split(/\W/)
+  a.reject! { |x| x == '' }
+  a.count
+  a.each { |x| x.downcase! }
+  hash = {}
+  a.each do |word|
+    hash[word] = 0 if hash[word].nil?
+    hash[word] += 1
+  end
+  hash.sort.to_h
+  # raise NotImplementedError
 end
